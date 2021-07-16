4f8414c68a2ea07ff45773e8ce93c476a72fba23
ParthSharma1234 <79687615+ParthSharma1234@users.noreply.github.com>
2021-07-16 15:58:08 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,14 @@
 # Changing and adding Dictionary Elements
-d1 = {'name': 'Parth', 'age': 19}
+d1 = {'name': 'xyz', 'age': 22}
 print(d1)
 
 # add item
 d1['address'] = 'Tokoyo'
 
-d2 = {'name':'Sahil', 'age': 19}
-d3 = {'name':'Ballu', 'age': 19}
-d4 = {'name':'Prashant', 'age': 19}
-d5 = {'name':'Yuvraj', 'age': 20}
+d2 = {'name':'A', 'age': 19}
+d3 = {'name':'B', 'age': 19}
+d4 = {'name':'P', 'age': 19}
+d5 = {'name':'Y', 'age': 20}
 
 print(d1)
 print(d2)
