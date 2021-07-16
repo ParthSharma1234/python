896fbafbfab6a4f9b53347273fd7542d86a6db3b
ParthSharma1234 <79687615+ParthSharma1234@users.noreply.github.com>
2021-07-16 16:00:25 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,4 +1,4 @@
-mystr = "Parth is a good boy"
+mystr = "A is a good boy"
 print(mystr) #This will print value inside mystr variable
 print(mystr[4])#This will print the variable on the 4th index position
 print(mystr[0:5])# here 0 is including and 5 is excluding
