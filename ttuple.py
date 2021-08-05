47e8d2e667b9064b237fe1e9b0027d3d31603e4e
ParthSharma1234 <79687615+ParthSharma1234@users.noreply.github.com>
2021-08-05 22:32:40 +0530

    Add files via upload

---
+++ ttuple.py
@@ -0,0 +1,19 @@
+#updating tuple
+"""tup1 = (12, 34.56);
+tup2 = ('abc', 'xyz');
+
+# Following action is not valid for tuples
+# tup1[0] = 100;
+
+# So let's create a new tuple as follows
+tup3 = tup1 + tup2;
+print (tup3);"""
+
+
+#deleting a tuple
+tup = ('physics', 'chemistry', 1997, 2000);
+print (tup);
+del tup;
+print ("After deleting tup : ");
+print (tup);
+
